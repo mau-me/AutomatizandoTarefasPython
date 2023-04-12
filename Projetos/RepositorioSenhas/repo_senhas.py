458db7848cc4ad881c8b7ba44e979852d1a31896
Mau-Me <mauriciosm95@gmail.com>
2023-04-12 01:24:33 -0300

    Feat: Implements password copy to clipboard.

--- Projetos/RepositorioSenhas/repo_senhas.py
+++ Projetos/RepositorioSenhas/repo_senhas.py
@@ -3,8 +3,30 @@
  # @ Email: mauriciosm95@gmail.com
  # @ GitHub: https://github.com/mau-me
  # @ Create Time: 2023-04-11 20:13:58
- # @ Description:
+ # @ Description: Programa para armazenamento de senhas de forma centralizada.
  '''
 
 #! /usr/bin/python3
 
+SENHAS = {
+    'email': 'F7minlBDDuvMJuxESSKHFhTxFtjVB6',
+    'blog': 'VmALvQyKAxiVH5G8v01if1MLZF3sdt',
+    'homepage': 'lsdflkjlASJDKjasfhaskfhaiuqry'
+}
+
+import sys
+import pyperclip
+
+if len(sys.argv) < 2:
+    print('Use: python repo_senhas.py[conta] - Copia a senha da conta')
+    sys.exit()
+
+conta = sys.argv[1]
+
+if conta not in SENHAS:
+  print('Senha não cadastrada para a conta ' + conta)
+  sys.exit()
+
+pyperclip.copy(SENHAS[conta])
+
+print('Senha copiada com sucesso')
